d30f6e80a6a2f19c255a27a4d920446b472a3a8d
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-11 17:16:48 +0200

    Add compare option

--- src/main.js
+++ src/main.js
@@ -3,13 +3,35 @@ const cp = require('child_process');
 const fs = require('fs');
 const px = require('path');
 
-let output = vscode.window.createOutputChannel('mCRL2');
+const output = vscode.window.createOutputChannel('mCRL2');
+
+const equivalenceTypes = {
+	'Weak Trace Equivalence': 'weak-trace',
+	'Strong Trace Equivalence': 'trace',
+	'Weak Bisimilarity': 'weak-bisim',
+	'Strong Bisilimarity (O(mlogn))': 'bisim',
+	'Strong Bisilimarity (O(mn))': 'bisim-gv',
+	'Strong Bisilimarity (O(mlogm))': 'bisim-gjkw',
+	'Branching Bisilimarity (O(mlogn))': 'branching-bisim',
+	'Branching Bisilimarity (O(mn))': 'branching-bisim-gv',
+	'Branching Bisilimarity (O(mlogm))': 'branching-bisim-gjkw',
+	'Divergence-preserving Branching Bisilimarity (O(mlogn))': 'dpbranching-bisim',
+	'Divergence-preserving Branching Bisilimarity (O(mn))': 'dpbranching-bisim-gv',
+	'Divergence-preserving Branching Bisilimarity (O(mlogm))': 'dpbranching-bisim-gjkw',
+	'Divergence-preserving Weak Bisimilarity': 'dpweak-bisim',
+	'Strong Simulation Equivalence': 'sim',
+	'Strong Ready Simulation Equivalence': 'ready-sim',
+	'Coupled Simulation Equivalence': 'coupled-sim',
+};
+
+const equivalenceNames = Object.keys(equivalenceTypes);
 
 function activate(context) {
 	register(context, 'mcrl2.parse', parse);
 	register(context, 'mcrl2.showGraph', showGraph);
 	register(context, 'mcrl2.simulate', simulate);
 	register(context, 'mcrl2.verifyProperties', verifyProperties);
+	register(context, 'mcrl2.equivalence', equivalence);
 }
 
 function deactivate() {
@@ -24,6 +46,47 @@ function register(context, name, func) {
 		func();
 	});
 	context.subscriptions.push(disposable);
+}
+
+function equivalence() {
+	const dir = toProjectPath();
+	const files = Array.from(getFiles(dir, '.mcrl2'));
+	const names = [];
+	const map = {};
+
+	for (let i = 0; i < files.length; ++i) {
+		const file = files[i];
+		const name = px.relative(dir, file);
+		names.push(name);
+		map[name] = file;
+	}
+
+	vscode.window.showQuickPick(names).then(x => {
+		const file = map[x];
+
+		vscode.window.showQuickPick(equivalenceNames).then(x => {
+			const equivalence = equivalenceTypes[x];
+
+			runMcrl2('mcrl22lps', [vscode.window.activeTextEditor.document.fileName, toProjectPath('./out/temp.lps')], () => {
+				runMcrl2('lps2lts', [toProjectPath('./out/temp.lps'), toProjectPath('./out/temp.lts')], () => {
+					runMcrl2('mcrl22lps', [file, toProjectPath('./out/temp2.lps')], () => {
+						runMcrl2('lps2lts', [toProjectPath('./out/temp2.lps'), toProjectPath('./out/temp2.lts')], () => {
+							runMcrl2('ltscompare', ['-c', '--equivalence=' + equivalence, toProjectPath('./out/temp.lts'), toProjectPath('./out/temp2.lts')], (result) => {
+								const lines = result.split(/\r?\n/);
+								const line = lines[lines.length - 1];
+								if (line === 'false') {
+									fs.copyFileSync('Counterexample0.trc', toProjectPath('./out/Counterexample0.trc'));
+									fs.unlinkSync('Counterexample0.trc');
+									output.appendLine('\nCounter Example Trace:');
+									runMcrl2('tracepp', [toProjectPath('./out/Counterexample0.trc')]);
+								}
+							});
+						});
+					});
+				});
+			});
+		});
+	});
 }
 
 function parse() {
@@ -106,7 +169,7 @@ function createCommand(cmd) {
 	return '"' + px.join(px.normalize(binPath), cmd.trim()) + '"';
 }
 
-function run(cmd, callback=function(){}, suppressed=false) {
+function run(cmd, callback=function(){}, suppressed=false, errorCallback=function(){}) {
 	let process = cp.exec(cmd);
 	var result = '';
 	process.stdout.setEncoding('utf8');
@@ -126,11 +189,13 @@ function run(cmd, callback=function(){}, suppressed=false) {
 	process.on('close', function(code) {
 		if (code == 0) {
 			callback(result.trim());
+		} else {
+			errorCallback(result.trim());
 		}
 	});
 }
 
-function runMcrl2(cmd, args, callback=function(){}, suppressed=false) {
+function runMcrl2(cmd, args, callback=function(){}, suppressed=false, errorCallback=function(){}) {
 	vscode.window.activeTextEditor.document.save().then(x => {
 		let argString = args.map(x => {
 			let trimmed = x.trim();
@@ -140,7 +205,7 @@ function runMcrl2(cmd, args, callback=function(){}, suppressed=false) {
 
 			return trimmed;
 		}).join(' ');
-		run(createCommand(cmd) + ' ' + argString, callback, suppressed);
+		run(createCommand(cmd) + ' ' + argString, callback, suppressed, errorCallback);
 	});
 }
 
